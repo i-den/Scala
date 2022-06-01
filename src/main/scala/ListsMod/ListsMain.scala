e92885566cb6ed2987cd16b85817ab6b99138127
iVan <24753710+i-den@users.noreply.github.com>
2022-06-01 17:43:33 +0300

    Collections

---
+++ src/main/scala/ListsMod/ListsMain.scala
@@ -0,0 +1,26 @@
+package com.idenchev.exercises.fp
+package ListsMod
+
+object ListsMain extends App {
+
+  val fruits = "apples" :: ("oranges" :: ("pears" :: Nil)) // List(apples, oranges, pears)
+
+  val others = List("1", "2", "3")
+
+  val thirds = fruits ::: others
+  // List(apples, oranges, pears, 1, 2, 3)
+
+  thirds.zipWithIndex
+  // List((apples,0), (oranges,1), (pears,2), (1,3), (2,4), (3,5))
+
+  thirds.mkString("[", ",", "]")
+  // [apples,oranges,pears,1,2,3]
+
+  val z = List.range(1, 5) // List(1, 2, 3, 4)
+
+  List(1, 2, 3, 4, 5).partition(_ % 2 == 0)
+  // (List(2, 4),List(1, 3, 5))
+
+  val s = List("a", "b", "c", "c").toSet
+  println(s)
+}
